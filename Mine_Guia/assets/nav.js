1e7602f4d43cdda4317bda81b4682ede5727fbec
Dudisdu99 <duduflesch2007@gmail.com>
2023-08-02 21:55:07 -0300

    muita coisa

--- Mine_Guia/assets/nav.js
+++ Mine_Guia/assets/nav.js
@@ -1,6 +1,20 @@
-let bt = document.querySelector(button.home)
-let navs = document.querySelector(div.navs)
+document.addEventListener('DOMContentLoaded', function() {
+  const bt = document.getElementById('mostrarNav');
+  const abas = document.querySelector('.abas');
+  const img = document.querySelector('.mostrarNav>img')
+  const label = document.querySelector('.mostrarNav')
 
-bt.addEventListener('click', function() {
-  navs.style.visibility = 'visible';
+  abas.classList.add('escondido'); // Inicia com a classe escondido
+
+  bt.addEventListener('change', function() {
+    if (bt.checked) {
+      abas.classList.remove('escondido'); 
+      img.src = 'img/3linhasBranco.png'
+      label.classList.add('selecionado')
+    } else {
+      abas.classList.add('escondido');
+      img.src = 'img/3linhasVermelho.png'
+      label.classList.remove('selecionado')
+    }
+  });
 });
