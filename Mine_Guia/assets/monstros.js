edcc96176c26777c99c77bd4187e8ff29563fc5a
Dudisdu99 <duduflesch2007@gmail.com>
2023-08-06 18:21:18 -0300

    muita coisa novamente (ifreme)

--- Mine_Guia/assets/monstros.js
+++ Mine_Guia/assets/monstros.js
@@ -1,18 +1,59 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const lista = document.querySelector(".lista")
+    const divIframes = document.querySelector('.iframes')
 
-    async function main() {
-        const request = await fetch("monstros.json")
-        const monstros = await request.json()
+    const mostrarAfogado = document.getElementById('mostrarInfoAfogado')
+    const iframeAfogado = document.querySelector('.iframeAfogado')
 
-        var nMonstros
+    const mostrarZumbi = document.getElementById('mostrarInfoZumbi')
+    const iframeZumbi = document.querySelector('.iframeZumbi')
 
-        function carregarMonstros() {
-            nMonstros = 0
+    const mostrarCreeper = document.getElementById('mostrarInfoCreeper')
+    const iframeCreeper = document.querySelector('.iframeCreeper')
 
-            if(nMonstros<monstros.length)[
-                monstros[nMonstros].monstro.forEach(alt => lista.innerHTML += `<div>${alt}</div>`)
-            ]
+    const mostrarEsqueleto = document.getElementById('mostrarInfoEsqueleto')
+    const iframeEsqueleto = document.querySelector('.iframeEsqueleto')
+
+    divIframes.classList.add('iframeEscondido');
+
+    iframeAfogado.classList.add('iframeEscondido');
+    iframeZumbi.classList.add('iframeEscondido');
+    iframeCreeper.classList.add('iframeEscondido');
+    iframeEsqueleto.classList.add('iframeEscondido');
+
+    mostrarAfogado.addEventListener('change', function() {
+        if (mostrarAfogado.checked) {
+            divIframes.classList.remove('iframeEscondido'); 
+            iframeAfogado.classList.remove('iframeEscondido');
+        } else {
+            divIframes.classList.add('iframeEscondido'); 
+            iframeAfogado.classList.add('iframeEscondido');
         }
-    }
+    });
+    mostrarZumbi.addEventListener('change', function() {
+        if (mostrarZumbi.checked) {
+            divIframes.classList.remove('iframeEscondido'); 
+            iframeZumbi.classList.remove('iframeEscondido');
+        } else {
+            divIframes.classList.add('iframeEscondido'); 
+            iframeZumbi.classList.add('iframeEscondido');
+        }
+    });
+    mostrarCreeper.addEventListener('change', function() {
+        if (mostrarCreeper.checked) {
+            divIframes.classList.remove('iframeEscondido'); 
+            iframeCreeper.classList.remove('iframeEscondido');
+        } else {
+            divIframes.classList.add('iframeEscondido'); 
+            iframeCreeper.classList.add('iframeEscondido');
+        }
+    });
+    mostrarEsqueleto.addEventListener('change', function() {
+        if (mostrarEsqueleto.checked) {
+            divIframes.classList.remove('iframeEscondido'); 
+            iframeEsqueleto.classList.remove('iframeEscondido');
+        } else {
+            divIframes.classList.add('iframeEscondido'); 
+            iframeEsqueleto.classList.add('iframeEscondido');
+        }
+    });
 });
