0cd8f9267a5dd185cf09d9f482e5fda52ce89c45
Dudisdu99 <duduflesch2007@gmail.com>
2023-08-19 18:43:58 -0300

    alterei muitas funçoes do js modal e mudei o json

--- assets/js/monstros.js
+++ assets/js/monstros.js
@@ -1,55 +1,64 @@
 const listaCriaturas = document.querySelector(".lista");
-const listaIframes = document.querySelector(".iframes");
 const modal = document.querySelector('.modal');
-const p = modal.querySelector('.info');
+const infoMobs = modal.querySelector('.info');
 const close = modal.querySelector('.close');
 const h1Modal = modal.querySelector('.nameMobModal');
 const divImgMobModal = modal.querySelector('.divImgMobModal');
+const statusMobModal = modal.querySelector('.statusMobModal > p');
+const dropMobModal = modal.querySelector('.dropMobModal');
 
 async function carregarBt() {
-    const request = await fetch("assets/json/monstros.json");
-    const monstros = await request.json();
+  try {
+    const response = await fetch("assets/json/monstros copy.json");
+    const monstros = await response.json();
 
-    let tipoCriatura = 0; // monstro 
-
-    function carregarCriaturas(tipoCriatura) {
-        listaCriaturas.innerHTML = "";
-        for (var i = 0; i < monstros[tipoCriatura].todosMonstros.length; i++) {
-            listaCriaturas.innerHTML +=
-                `<button class="criaturas" id="${monstros[tipoCriatura].inpId[i]}">
-                    <img src="${monstros[tipoCriatura].img[i]}"></img>
-                    <div class="nameMobLista">${monstros[tipoCriatura].todosMonstros[i]}</div>
-                </button>`;
-        }
+    function carregarCriaturas() {
+      listaCriaturas.innerHTML = "";
+      monstros.forEach(monstro => {
+        listaCriaturas.innerHTML +=
+          `<button class="criaturas" id="${monstro.inpId}">
+              <img src="${monstro.img}"></img>
+              <div class="nameMobLista">${monstro.nome}</div>
+          </button>`;
+      });
     }
 
-    carregarCriaturas(0);
+    carregarCriaturas();
 
-    const listaBt = [
-        document.getElementById('mostrarInfoAfogado'),
-        document.getElementById('mostrarInfoZumbi'),
-        document.getElementById('mostrarInfoCreeper'),
-        document.getElementById('mostrarInfoEsqueleto')
-    ];
+    const listaBt = monstros.map(monstro => document.getElementById(monstro.inpId));
 
     listaBt.forEach((bt, index) => {
-        bt.addEventListener('click', () => {
-            modal.classList.add("opened");
-            h1Modal.innerHTML = monstros[tipoCriatura].todosMonstros[index];
-            divImgMobModal.innerHTML = `<img src="${monstros[tipoCriatura].img[index]}" class="imgMobModal"></img>`;
-            p.innerHTML = monstros[tipoCriatura].info[index];
+      bt.addEventListener('click', () => {
+        modal.classList.add("opened");
+        h1Modal.innerHTML = monstros[index].nome;
+        divImgMobModal.innerHTML = `<img src="${monstros[index].img}" class="imgMobModal"></img>`;
+        infoMobs.innerHTML = monstros[index].info;
+        statusMobModal.innerHTML = monstros[index].status;
+        dropMobModal.innerHTML = "";
+
+        monstros[index].drop.forEach(drop => {
+          const dropElement = document.createElement("div");
+          dropElement.className = "dropsMobModal";
+          dropElement.innerHTML = `
+            ${drop.nome} <img src="${drop.img}" alt="${drop.nome} drop">
+          `;
+          dropMobModal.appendChild(dropElement);
         });
+      });
     });
 
     close.addEventListener('click', () => {
-        modal.classList.remove("opened");
+      modal.classList.remove("opened");
     });
 
     modal.addEventListener('click', (e) => {
-        if (e.target === modal) {
-            modal.classList.remove("opened");
-        }
+      if (e.target === modal) {
+        modal.classList.remove("opened");
+      }
     });
+  } catch (error) {
+    console.error('Erro ao carregar elementos do JSON:', error);
+  }
 }
 
 carregarBt();
