2b71474477ee100a18311d90617c42bfed98ad08
Dudisdu99 <duduflesch2007@gmail.com>
2023-08-19 16:26:13 -0300

    porsonalizei o modal

--- assets/js/monstros.js
+++ assets/js/monstros.js
@@ -1,25 +1,55 @@
-const divIframes = document.querySelector('.iframes');
+const listaCriaturas = document.querySelector(".lista");
+const listaIframes = document.querySelector(".iframes");
+const modal = document.querySelector('.modal');
+const p = modal.querySelector('.info');
+const close = modal.querySelector('.close');
+const h1Modal = modal.querySelector('.nameMobModal');
+const divImgMobModal = modal.querySelector('.divImgMobModal');
 
-const elementosInfo = [
-    { bt: document.getElementById('mostrarInfoAfogado')},
-    { bt: document.getElementById('mostrarInfoZumbi')},
-    { bt: document.getElementById('mostrarInfoCreeper')},
-    { bt: document.getElementById('mostrarInfoEsqueleto')}
-];
+async function carregarBt() {
+    const request = await fetch("assets/json/monstros.json");
+    const monstros = await request.json();
 
-divIframes.classList.add('escondido');
-elementosInfo.forEach(elemento => elemento.iframe.classList.add('escondido'))
+    let tipoCriatura = 0; // monstro 
 
-elementosInfo.forEach(elemento => {
-    elemento.bt.addEventListener('change', () => {
-        if (elemento.bt.checked) {
-            divIframes.classList.remove('escondido')
-            elemento.iframe.classList.remove('escondido')
-        } else {
-            if (elementosInfo.every(item => !item.bt.checked)) {
-                divIframes.classList.add('escondido')
-            }
-            elemento.iframe.classList.add('escondido')
+    function carregarCriaturas(tipoCriatura) {
+        listaCriaturas.innerHTML = "";
+        for (var i = 0; i < monstros[tipoCriatura].todosMonstros.length; i++) {
+            listaCriaturas.innerHTML +=
+                `<button class="criaturas" id="${monstros[tipoCriatura].inpId[i]}">
+                    <img src="${monstros[tipoCriatura].img[i]}"></img>
+                    <div class="nameMobLista">${monstros[tipoCriatura].todosMonstros[i]}</div>
+                </button>`;
         }
-    })
-})
+    }
+
+    carregarCriaturas(0);
+
+    const listaBt = [
+        document.getElementById('mostrarInfoAfogado'),
+        document.getElementById('mostrarInfoZumbi'),
+        document.getElementById('mostrarInfoCreeper'),
+        document.getElementById('mostrarInfoEsqueleto')
+    ];
+
+    listaBt.forEach((bt, index) => {
+        bt.addEventListener('click', () => {
+            modal.classList.add("opened");
+            h1Modal.innerHTML = monstros[tipoCriatura].todosMonstros[index];
+            divImgMobModal.innerHTML = `<img src="${monstros[tipoCriatura].img[index]}" class="imgMobModal"></img>`;
+            p.innerHTML = monstros[tipoCriatura].info[index];
+        });
+    });
+
+    close.addEventListener('click', () => {
+        modal.classList.remove("opened");
+    });
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.classList.remove("opened");
+        }
+    });
+}
+
+carregarBt();
