3ddd02fae8c7ddbdbba3feded33e5f3be03cc510
Dudisdu99 <131018122+Dudisdud99@users.noreply.github.com>
2023-08-26 15:04:56 +0000

    add js para animais, blocos e outros html

---
+++ assets/js/blocos.js
@@ -0,0 +1,7 @@
+const screenWidth = window.screen.width;
+const screenHeight = window.screen.height;
+const fundo = document.querySelector('.fundo')
+
+if(screenWidth<768){
+    fundo.src = "assets/img/mobsCelular.jpg"
+}
