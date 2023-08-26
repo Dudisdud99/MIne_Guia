3d2bd221d6883cbcf6ead82b5dfb7ba957da52dd
Dudisdu99 <131018122+Dudisdud99@users.noreply.github.com>
2023-08-26 15:56:00 +0000

    treminei o trailer e img fundo para celular

--- assets/js/blocos.js
+++ assets/js/blocos.js
@@ -3,5 +3,5 @@ const screenHeight = window.screen.height;
 const fundo = document.querySelector('.fundo')
 
 if(screenWidth<768){
-    fundo.src = "assets/img/blocosCelular.webp"
+    fundo.src = "assets/img/blocosCelular.jpg"
 }
