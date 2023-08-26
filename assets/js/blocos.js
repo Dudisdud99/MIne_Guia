a16eee13448ebdd425871fd01b7e06ea210a2a5b
Dudisdu99 <131018122+Dudisdud99@users.noreply.github.com>
2023-08-26 23:00:27 +0000

    deixando o js para versao celular  melhor

--- assets/js/blocos.js
+++
@@ -1,7 +0,0 @@
-const screenWidth = window.screen.width;
-const screenHeight = window.screen.height;
-const fundo = document.querySelector('.fundo')
-
-if(screenWidth<768){
-    fundo.src = "assets/img/blocosCelular.jpg"
-}
---
+++ assets/js/imgCelular.js
@@ -0,0 +1,19 @@
+const screenWidth = window.screen.width;
+const screenHeight = window.screen.height;
+const fundo = document.querySelector('.fundo')
+
+if(screenWidth<768){
+    if (window.location.pathname.includes("animais.html")) {
+        fundo.src = "assets/img/animaisCelular.jpg";
+    }
+    if (window.location.pathname.includes("blocos.html")) {
+        fundo.src = "assets/img/blocosCelular.jpg"
+    }
+    if (window.location.pathname.includes("index.html")) {
+        fundo.src = "assets/img/indexCelular.webp"
+    }
+    if (window.location.pathname.includes("monstros.html")) {
+        fundo.src = "assets/img/mobsCelular.jpg"
+    }    
+}
+
