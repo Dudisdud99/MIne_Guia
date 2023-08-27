df743104d73076f93e6a6c11d50448cb1a1bb6e5
Dudisdu99 <131018122+Dudisdud99@users.noreply.github.com>
2023-08-27 20:49:49 +0000

    add json dos mobs hostis, etc,  ao monstros.js

--- assets/js/monstros.js
+++ assets/js/monstros.js
@@ -1,4 +1,3 @@
-const listaCriaturas = document.querySelector(".lista");
 const modal = document.querySelector('.modal');
 const infoMobs = modal.querySelector('.info');
 const close = modal.querySelector('.close');
@@ -6,16 +5,23 @@ const h1Modal = modal.querySelector('.nameMobModal');
 const divImgMobModal = modal.querySelector('.divImgMobModal');
 const statusMobModal = modal.querySelector('.statusMobModal > p');
 const dropMobModal = modal.querySelector('.dropMobModal');
+const listaTodos = document.querySelector(".todos");
+const listaHostis = document.querySelector(".hostis");
+const listaNeutros = document.querySelector(".neutros");
+const listaNether = document.querySelector(".nether");
+const listaChefoes = document.querySelector(".chefoes");
 
-async function carregarBt() {
+//--TODOS--
+
+async function carregarBtTodos() {
   try {
-    const response = await fetch("assets/json/monstros.json");
+    const response = await fetch("assets/json/monstrosTodos.json");
     const monstros = await response.json();
 
     function carregarCriaturas() {
-      listaCriaturas.innerHTML = "";
+      listaTodos.innerHTML = "";
       monstros.forEach(monstro => {
-        listaCriaturas.innerHTML +=
+        listaTodos.innerHTML +=
           `<button class="criaturas" id="${monstro.inpId}">
               <img src="${monstro.img}"></img>
               <div class="nameMobLista">${monstro.nome}</div>
@@ -61,6 +67,239 @@ async function carregarBt() {
   }
 }
 
-carregarBt();
 
-window.onload = carregarBt();
+//--HOSTIS--
+
+async function carregarBtHostis() {
+  try {
+    const response = await fetch("assets/json/monstrosHostis.json");
+    const monstros = await response.json();
+
+    function carregarCriaturas() {
+      listaHostis.innerHTML = "";
+      monstros.forEach(monstro => {
+        listaHostis.innerHTML +=
+          `<button class="criaturas" id="${monstro.inpId}">
+              <img src="${monstro.img}"></img>
+              <div class="nameMobLista">${monstro.nome}</div>
+          </button>`;
+      });
+    }
+
+    carregarCriaturas();
+
+    const listaBt = monstros.map(monstro => document.getElementById(monstro.inpId));
+
+    listaBt.forEach((bt, index) => {
+      bt.addEventListener('click', () => {
+        modal.classList.add("opened");
+        h1Modal.innerHTML = monstros[index].nome;
+        divImgMobModal.innerHTML = `<img src="${monstros[index].img}" class="imgMobModal"></img>`;
+        infoMobs.innerHTML = monstros[index].info;
+        statusMobModal.innerHTML = monstros[index].status;
+        dropMobModal.innerHTML = "";
+
+        monstros[index].drop.forEach(drop => {
+          const dropElement = document.createElement("div");
+          dropElement.className = "dropsMobModal";
+          dropElement.innerHTML = `
+            ${drop.nome} <img src="${drop.img}" alt="${drop.nome} drop">
+          `;
+          dropMobModal.appendChild(dropElement);
+        });
+      });
+    });
+
+    close.addEventListener('click', () => {
+      modal.classList.remove("opened");
+    });
+
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        modal.classList.remove("opened");
+      }
+    });
+  } catch (error) {
+    console.error('Erro ao carregar elementos do JSON:', error);
+  }
+}
+
+//--NEUTROS--
+
+async function carregarBtNeutros() {
+  try {
+    const response = await fetch("assets/json/monstrosNeutros.json");
+    const monstros = await response.json();
+
+    function carregarCriaturas() {
+      listaNeutros.innerHTML = "";
+      monstros.forEach(monstro => {
+        listaNeutros.innerHTML +=
+          `<button class="criaturas" id="${monstro.inpId}">
+              <img src="${monstro.img}"></img>
+              <div class="nameMobLista">${monstro.nome}</div>
+          </button>`;
+      });
+    }
+
+    carregarCriaturas();
+
+    const listaBt = monstros.map(monstro => document.getElementById(monstro.inpId));
+
+    listaBt.forEach((bt, index) => {
+      bt.addEventListener('click', () => {
+        modal.classList.add("opened");
+        h1Modal.innerHTML = monstros[index].nome;
+        divImgMobModal.innerHTML = `<img src="${monstros[index].img}" class="imgMobModal"></img>`;
+        infoMobs.innerHTML = monstros[index].info;
+        statusMobModal.innerHTML = monstros[index].status;
+        dropMobModal.innerHTML = "";
+
+        monstros[index].drop.forEach(drop => {
+          const dropElement = document.createElement("div");
+          dropElement.className = "dropsMobModal";
+          dropElement.innerHTML = `
+            ${drop.nome} <img src="${drop.img}" alt="${drop.nome} drop">
+          `;
+          dropMobModal.appendChild(dropElement);
+        });
+      });
+    });
+
+    close.addEventListener('click', () => {
+      modal.classList.remove("opened");
+    });
+
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        modal.classList.remove("opened");
+      }
+    });
+  } catch (error) {
+    console.error('Erro ao carregar elementos do JSON:', error);
+  }
+}
+
+//--NETHER--
+
+async function carregarBtNether() {
+  try {
+    const response = await fetch("assets/json/monstrosNether.json");
+    const monstros = await response.json();
+
+    function carregarCriaturas() {
+      listaNether.innerHTML = "";
+      monstros.forEach(monstro => {
+        listaNether.innerHTML +=
+          `<button class="criaturas" id="${monstro.inpId}">
+              <img src="${monstro.img}"></img>
+              <div class="nameMobLista">${monstro.nome}</div>
+          </button>`;
+      });
+    }
+
+    carregarCriaturas();
+
+    const listaBt = monstros.map(monstro => document.getElementById(monstro.inpId));
+
+    listaBt.forEach((bt, index) => {
+      bt.addEventListener('click', () => {
+        modal.classList.add("opened");
+        h1Modal.innerHTML = monstros[index].nome;
+        divImgMobModal.innerHTML = `<img src="${monstros[index].img}" class="imgMobModal"></img>`;
+        infoMobs.innerHTML = monstros[index].info;
+        statusMobModal.innerHTML = monstros[index].status;
+        dropMobModal.innerHTML = "";
+
+        monstros[index].drop.forEach(drop => {
+          const dropElement = document.createElement("div");
+          dropElement.className = "dropsMobModal";
+          dropElement.innerHTML = `
+            ${drop.nome} <img src="${drop.img}" alt="${drop.nome} drop">
+          `;
+          dropMobModal.appendChild(dropElement);
+        });
+      });
+    });
+
+    close.addEventListener('click', () => {
+      modal.classList.remove("opened");
+    });
+
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        modal.classList.remove("opened");
+      }
+    });
+  } catch (error) {
+    console.error('Erro ao carregar elementos do JSON:', error);
+  }
+}
+
+//--CHEFOES--
+
+async function carregarBtChefoes() {
+  try {
+    const response = await fetch("assets/json/monstrosChefoes.json");
+    const monstros = await response.json();
+
+    function carregarCriaturas() {
+      listaChefoes.innerHTML = "";
+      monstros.forEach(monstro => {
+        listaChefoes.innerHTML +=
+          `<button class="criaturas" id="${monstro.inpId}">
+              <img src="${monstro.img}"></img>
+              <div class="nameMobLista">${monstro.nome}</div>
+          </button>`;
+      });
+    }
+
+    carregarCriaturas();
+
+    const listaBt = monstros.map(monstro => document.getElementById(monstro.inpId));
+
+    listaBt.forEach((bt, index) => {
+      bt.addEventListener('click', () => {
+        modal.classList.add("opened");
+        h1Modal.innerHTML = monstros[index].nome;
+        divImgMobModal.innerHTML = `<img src="${monstros[index].img}" class="imgMobModal"></img>`;
+        infoMobs.innerHTML = monstros[index].info;
+        statusMobModal.innerHTML = monstros[index].status;
+        dropMobModal.innerHTML = "";
+
+        monstros[index].drop.forEach(drop => {
+          const dropElement = document.createElement("div");
+          dropElement.className = "dropsMobModal";
+          dropElement.innerHTML = `
+            ${drop.nome} <img src="${drop.img}" alt="${drop.nome} drop">
+          `;
+          dropMobModal.appendChild(dropElement);
+        });
+      });
+    });
+
+    close.addEventListener('click', () => {
+      modal.classList.remove("opened");
+    });
+
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        modal.classList.remove("opened");
+      }
+    });
+  } catch (error) {
+    console.error('Erro ao carregar elementos do JSON:', error);
+  }
+}
+
+carregarBtTodos();
+carregarBtHostis();
+carregarBtNeutros();
+carregarBtNether();
+carregarBtChefoes();
+
+window.onload = carregarBtTodos();
+window.onload = carregarBtHostis();
+window.onload = carregarBtNeutros();
+window.onload = carregarBtNether();
+window.onload = carregarBtChefoes();
